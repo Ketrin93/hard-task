cc26bb901ea7aa6adc3044314dc86d8570d83f7b
hello <lazareva_ekaterina_96_96@mail.ru>
2025-02-11 05:42:06 +0900

    DZ-2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     String firstName;
     String lastName;
@@ -14,5 +16,21 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return  firstName + " " +  lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Author author)) return false;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         Author janette = new Author("Janette", "Wals");
-        Author hayleyScrivenor = new Author("Hayley", "Scrivenor");
+        Author author = new Author("Hayley", "Scrivenor");
 
         Book glassCastle = new Book("glassCastle", 2020, janette);
-        Book dirtyCity = new Book("dirtyCity", 2025, hayleyScrivenor);
+        Book dirtyCity = new Book("dirtyCity", 2025, new Author("Hayley", " Scrivenor"));
+
+        System.out.println(dirtyCity.getAuthor().equals(author));
 
 
         System.out.println(janette.equals(glassCastle.getAuthor()));
